9bacd7f3535969e1cde5ec347c90c9bfb4fdc7ba
SILVANI CUSTODIO DE OLIVEIRA <silvani.oliveira@etec.sp.gov.br>
2021-03-07 12:26:52 -0300

    C# pronto

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace pc_etec1b_projeto_cidades_litoral_sul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Santos\nSão Vicente\nPraia Grande\nMongaguá\nItanhaém\nPerúibe\nIguape\nIlha Cumprida\nCananéia");
+        Console.ReadKey();
+                
+        }
+    }
+}
